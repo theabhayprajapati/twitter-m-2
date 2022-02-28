7f50e52ec20edcd4ca68f0692f07d5562275402c
theabhayprajapati <theabhayprajapati@gmail.com>
2022-02-28 15:43:54 +0530

    added feature of adding username, which ever user regester'

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,5 +8,13 @@ export default NextAuth({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET
         })
-    ]
+    ],
+    callbacks: {
+        async session({ session, token, user }) {
+            //   remove everthing after @ from email
+            session.user.username = session.user.email.split("@")[0]
+            session.user.uid = token.sub
+            return session
+        },
+    },
 })
