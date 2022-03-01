7d64ed7a127d44b777d7b7c3e8091d4ca5661a4c
theabhayprajapati <theabhayprajapati@gmail.com>
2022-03-01 16:36:12 +0530

    added mobile login feature:

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import { SparklesIcon, XIcon } from '@heroicons/react/outline'
 const HeaderSection = ({ title }: any) => {
   return (
 
-    <header className={` sticky z-50     opacity-95 top-0 h-14 text-white font-bold flex justify-start items-center`}>
+    <header className={` sticky z-50 opacity-95 top-0 h-14 text-white font-bold flex justify-start items-center`}>
       <button className="flex w-full justify-between">
         <h1 className="font-bold">{title}</h1>
         <SparklesIcon className='nav-icons' />
