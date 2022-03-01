6e565c6b587843380aaab10fae9acbdd02d3471c
theabhayprajapati <theabhayprajapati@gmail.com>
2022-03-02 00:58:04 +0530

    added feature showing total numeber of tweet user's have made from there ccount'

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,21 +6,24 @@ const HeaderSection = ({ title, hidden, tweets }: any) => {
   return (
 
     <header className={` sticky z-50 opac ity-95 top-0 h-14 text-white font-bold flex justify-start items-center`}>
-      <div className="flex w-full justify-between space-x-4">
+      <div className="flex w-full justify-start space-x-4">
         <button>
           <ArrowLeftIcon className={`input-btn h-[20px] ${!hidden ? 'hidden' : null}`} />
         </button>
-        <h1 className={`font-bold ${!hidden ? null : 'flex-1'}`}>{title}</h1>
+        <button className={`font-bold flex flex-col justify-start items-start ${hidden ? null : 'flex-1 self-start'}`}>
+          <h1>
+            {title}
+          </h1>
+          {
+            tweets && <h2 className="text-xs text-gray-300 opacity-70 font-medium">
+              {tweets} Tweets
+            </h2>
+
+          }
+        </button>
         <SparklesIcon className={`nav-icons ${hidden ? 'hidden' : null}`} />
       </div>
-      {
-        hidden &&
-        <button className="text-white">
-          <h1>
-            {tweets}
-          </h1>
-        </button>
-      }
+
     </header>
 
   )
